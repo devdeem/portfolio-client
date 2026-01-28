e155ac3f90511bf09b3f5754006fe188f2c3b6c6
Alexandr Virgovič <91419390+devdeem@users.noreply.github.com>
2026-01-28 14:02:56 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import Header from "@/components/Header";
-import About from "@/components/About";
 import TechStack from "@/components/TechStack";
 import Projects from "@/components/Projects";
+import Header from "@/components/Header";
+import About from "@/components/About";
 
 export default function Home() {
 	return (
